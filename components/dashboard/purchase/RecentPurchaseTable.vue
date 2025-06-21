9f109bd04d6744fd29d03d517130c8ec15cbca6d
roshan-se <roshan.adhikari.se@gmail.com>
2025-06-21 20:46:08 +0800

    added shadcn combobox ui

--- components/dashboard/purchase/RecentPurchaseTable.vue
+++ components/dashboard/purchase/RecentPurchaseTable.vue
@@ -45,7 +45,7 @@ const confirmDelete = async (value) => {
           </th>
           <th
             scope="col"
-            class="px-6 py-3">
+            class="px-6 py-3 text-center">
             Quantity
           </th>
           <th
@@ -75,7 +75,7 @@ const confirmDelete = async (value) => {
             {{ purchase.product?.name || "N/A" }}
           </td>
           <td
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            class="px-6 py-4 font-medium text-gray-900 text-center whitespace-nowrap dark:text-white">
             {{ purchase.quantity }}
           </td>
           <td
@@ -86,23 +86,7 @@ const confirmDelete = async (value) => {
             class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
             {{ new Date(purchase.created_at).toLocaleDateString() }}
           </td>
-          <td class="px-6 py-4 flex items-center gap-2">
-            <a
-              href="#"
-              class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-              </svg>
-            </a>
+          <td class="px-6 py-4 flex gap-2">
             <button
               @click="confirmDelete(purchase.id)"
               class="font-medium text-red-600 dark:text-red-500 hover:underline">
