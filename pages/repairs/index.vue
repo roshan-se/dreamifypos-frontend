c3ce902c154c91a468e2df5ec6c59415a16a17e7
roshan-se <roshan.adhikari.se@gmail.com>
2025-08-09 19:31:45 +0800

    Minor Icon changes on Repair Table

--- pages/repairs/index.vue
+++ pages/repairs/index.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
-import RepairsTable from '~/components/tables/RepairsTable.vue';
-
+import RepairsTable from "~/components/tables/RepairsTable.vue";
+import Button from "~/components/ui/button/Button.vue";
 </script>
 
 <template>
-    <div
-      class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-      <div class="flex justify-between items-center mb-6">
-          <h1 class="text-xl font-semibold uppercase">Repairs</h1>
+  <div
+    class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+    <div class="flex justify-between items-center mb-6">
+      <h1 class="text-xl font-semibold uppercase">Repairs</h1>
 
-          <NuxtLink to="/repairs/create" class="primary-btn">
-            Add Repair
-          </NuxtLink>
-        </div>
-
-        <RepairsTable />
+      <Button
+        asChild
+        class="bg-sky-600 hover:bg-sky-800 cursor-pointer"
+        variant="default">
+        <NuxtLink
+          to="/repairs/create"
+          class="">
+          Add Repair
+        </NuxtLink>
+      </Button>
     </div>
+
+    <RepairsTable />
+  </div>
 </template>
